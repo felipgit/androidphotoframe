0b663d3ae0c8126ef86300897360a7a28f4e276b
Kim Kaarlenkaski <kim@felip.se>
2020-04-12 09:57:13 +0200

    script to add crontab jobs

---
+++ crontab.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+# enable autostart of crontab daemon
+echo "crond -b" > /system/etc/init.d/crond
+
+# set permissions to execute crond script
+chmod 755 /system/etc/init.d/crond
+
+# make crontab folders, default location missing
+mkdir -p /system/etc/cron.d/crontabs
+
+# add cronjobs
+echo '#crontab for root' > /system/etc/cron.d/crontabs/root
+echo '0 * * * * /sdcard/framenewpictures.sh >> /sdcard/framenewpictures.log' >> /system/etc/cron.d/crontabs/root
+echo '0 0 1 * * rm /sdcard/framenewpictures.log' >> /system/etc/cron.d/crontabs/root
+crontab /system/etc/cron.d/crontabs/root
